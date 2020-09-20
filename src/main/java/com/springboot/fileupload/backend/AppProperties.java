2d8c964bfc1fcdfd7357eddfabbbfaf8d84c14db
Minwei Yao <dzrematt@gmail.com>
2020-09-21 03:31:59 +1000

    create server implementing TUS protocol

---
+++ src/main/java/com/springboot/fileupload/backend/AppProperties.java
@@ -0,0 +1,29 @@
+package com.springboot.fileupload.backend;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@ConfigurationProperties(prefix = "app")
+@Component
+public class AppProperties {
+
+	private String tusUploadDirectory;
+
+	private String appUploadDirectory;
+
+	public String getTusUploadDirectory() {
+		return this.tusUploadDirectory;
+	}
+
+	public void setTusUploadDirectory(String tusUploadDirectory) {
+		this.tusUploadDirectory = tusUploadDirectory;
+	}
+
+	public String getAppUploadDirectory() {
+		return this.appUploadDirectory;
+	}
+
+	public void setAppUploadDirectory(String appUploadDirectory) {
+		this.appUploadDirectory = appUploadDirectory;
+	}
+}
